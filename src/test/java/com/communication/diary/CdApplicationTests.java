d55e763d706ee884deed2f9d6121b466330397ef
jinyeong-afk <xoeh7002@naver.com>
2023-07-29 23:10:29 +0900

    [CREATE] Project Init

---
+++ src/test/java/com/communication/diary/CdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communication.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
